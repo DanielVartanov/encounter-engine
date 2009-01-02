94e66efd2fe43e536fca692ec1936b9664f3c505
aldor <daniel.vartanov@gmail.com>
2009-01-02 23:38:05 +0600

    Draft of login features and steps

---
+++ features/support/env.rb
@@ -0,0 +1,23 @@
+# Sets up the Merb environment for Cucumber (thanks to krzys and roman)
+require "rubygems"
+
+# Add the local gems dir if found within the app root; any dependencies loaded
+# hereafter will try to load from the local gems before loading system gems.
+if (local_gem_dir = File.join(File.dirname(__FILE__), '..', 'gems')) && $BUNDLE.nil?
+  $BUNDLE = true; Gem.clear_paths; Gem.path.unshift(local_gem_dir)
+end
+
+require "merb-core"
+require "spec"
+require "merb_cucumber/world/webrat"
+require "merb_cucumber/helpers/activerecord"
+
+# Uncomment if you want transactional fixtures
+# Merb::Test::World::Base.use_transactional_fixtures
+
+# Quick fix for post features running Rspec error, see 
+# http://gist.github.com/37930
+def Spec.run? ; true; end
+
+Merb.start_environment(:testing => true, :adapter => 'runner', :environment => ENV['MERB_ENV'] || 'test')
+  
