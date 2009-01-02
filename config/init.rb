aed3fe62745d97dcc206fa8ec6d105d88b3e1932
aldor <daniel.vartanov@gmail.com>
2009-01-02 15:22:15 +0600

    Application skeleton

---
+++ config/init.rb
@@ -0,0 +1,25 @@
+# Go to http://wiki.merbivore.com/pages/init-rb
+ 
+# Specify a specific version of a dependency
+# dependency "RedCloth", "> 3.0"
+
+ use_orm :activerecord
+use_test :rspec
+use_template_engine :erb
+ 
+Merb::Config.use do |c|
+  c[:use_mutex] = false
+  c[:session_store] = 'cookie'  # can also be 'memory', 'memcache', 'container', 'datamapper
+  
+  # cookie session store configuration
+  c[:session_secret_key]  = '317d0a0e1a381404dd8edc5527896ecd140d117d'  # required for cookie session store
+  c[:session_id_key] = '_encounter-engine_session_id' # cookie session id key, defaults to "_session_id"
+end
+ 
+Merb::BootLoader.before_app_loads do
+  # This will get executed after dependencies have been loaded but before your app's classes have loaded.
+end
+ 
+Merb::BootLoader.after_app_loads do
+  # This will get executed after your app's classes have been loaded.
+end
