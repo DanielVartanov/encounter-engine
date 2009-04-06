a09d7d4f9d1c724c7da691cc8936a21d9c25fbe3
Daniel Vartanov <daniel.vartanov@gmail.com>
2009-04-06 11:56:44 +0000

    Updated Merb to 1.0.10 and Cucumber to 0.2.0.2

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,8 +1,14 @@
-merb_gems_version = "1.0.9"
+merb_gems_version = "1.0.10"
 
+# For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
+dependency "merb-core", merb_gems_version
 dependency "merb-action-args", merb_gems_version
 dependency "merb-assets", merb_gems_version
-dependency "merb-cache", merb_gems_version
+dependency("merb-cache", merb_gems_version) do
+  Merb::Cache.setup do
+    register(Merb::Cache::FileStore) unless Merb.cache
+  end
+end
 dependency "merb-helpers", merb_gems_version
 dependency "merb-mailer", merb_gems_version
 dependency "merb-slices", merb_gems_version
@@ -16,7 +22,7 @@ dependency "do_sqlite3"
 
 dependency "activerecord", "2.2.2"
 
-dependency "webrat"
-dependency "nokogiri"
-dependency "cucumber", "0.1.16"
+#dependency "cucumber", "0.2.0.2"
+#dependency "webrat"
+#dependency "nokogiri"
 #dependency "david-merb_cucumber", "0.5.1.2"
--- lib/tasks/cucumber.rake
+++ lib/tasks/cucumber.rake
@@ -1,8 +1,16 @@
 require 'cucumber/rake/task'
 
 cucumber_options = lambda do |t|
-  t.cucumber_opts = "--language ru"
+#  t.cucumber_opts = "--language ru"
 end
 
 Cucumber::Rake::Task.new(:features, &cucumber_options)
 Cucumber::Rake::FeatureTask.new(:feature, &cucumber_options)
+namespace :merb_cucumber do
+  task :test_env do
+    Merb.start_environment(:environment => "test", :adapter => 'runner')
+  end
+end
+
+task :features => 'merb_cucumber:test_env'
+task :feature  => 'merb_cucumber:test_env'
