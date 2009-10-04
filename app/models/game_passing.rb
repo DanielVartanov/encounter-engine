528dad232f1426b52eeb2f1e64fcf9a8224ac68c
Daniel Vartanov <daniel.vartanov@gmail.com>
2009-10-04 18:45:39 +0600

    GamePassing#of(team, game), GamePassing#of_team

--- app/models/game_passing.rb
+++ app/models/game_passing.rb
@@ -4,7 +4,12 @@ class GamePassing < ActiveRecord::Base
   belongs_to :current_level, :class_name => "Level"
 
   named_scope :of_game, lambda { |game| { :conditions => { :game_id => game.id } } }
+  named_scope :of_team, lambda { |team| { :conditions => { :team_id => team.id } } }
   named_scope :finished, :conditions => ['finished_at IS NOT NULL']
+
+  def self.of(team, game)
+    self.of_team(team).of_game(game).first
+  end
 
   def check_answer!(answer)    
     if correct_answer?(answer)
