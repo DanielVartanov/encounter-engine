fcde2fce72f83d044569fa617d01d32a5bdf9885
Daniel Vartanov <daniel.vartanov@gmail.com>
2009-10-31 14:30:16 +0600

    Hint#ready_to_show?

--- app/models/hint.rb
+++ app/models/hint.rb
@@ -1,3 +1,8 @@
 class Hint < ActiveRecord::Base
   belongs_to :level
+
+  def ready_to_show?(current_level_entered_at)
+    seconds_passed = Time.now - current_level_entered_at    
+    seconds_passed >= self.delay
+  end
 end
