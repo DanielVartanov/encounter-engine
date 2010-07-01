41787bdbe66c1943367e3875f5da964d24d34543
nozim <nozim.mehrubonov@gmail.com>
2010-07-01 11:48:51 +0600

    Закончил тикет #94-"Отрефакторить некоторые части кода тикета #51"

--- app/controllers/game_entries.rb
+++ app/controllers/game_entries.rb
@@ -1,14 +1,13 @@
 class GameEntries < Application
   before :ensure_authenticated
-  before :find_game
-  before :find_team
+  before :find_game, :only=>:new
+  before :find_team, :only=>:new
+  before :find_entry, :exclude =>:new
 
   def new
     @game_entry = GameEntry.all(:conditions =>
-        { :team_id => @team.id, :game_id => @game.id }).first
-    if !@game_entry
-      @game_entry = GameEntry.new
-    end
+        {:team_id => @team.id, :game_id => @game.id}).first
+    @game_entry = GameEntry.new unless @game_entry
     @game_entry.status = "new"
     @game_entry.game = @game
     @game_entry.team_id = @team.id
@@ -17,18 +16,14 @@ class GameEntries < Application
   end
   
   def accept
-    @game_entry = GameEntry.all(:conditions =>
-        { :team_id => @team.id, :game_id => @game.id }).first
-    @game_entry.status = "accepted"
-    @game_entry.save
+    @entry.status = "accepted"
+    @entry.save
     redirect url(:dashboard)
   end
   
   def reject
-    @game_entry = GameEntry.all(:conditions =>
-        { :team_id => @team.id, :game_id => @game.id }).first
-    @game_entry.status = "rejected"
-    @game_entry.save
+    @entry.status = "rejected"
+    @entry.save
     redirect url(:dashboard)
   end
 
@@ -39,5 +34,8 @@ protected
   
   def find_team
     @team = Team.find(params[:team_id])
+  end
+  def find_entry
+    @entry=GameEntry.find(params[:id])
   end
 end
