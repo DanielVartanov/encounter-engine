3013d4c48ed2a8ba376ef3234c8166b6c8a30931
nozim <nozim.mehrubonov@gmail.com>
2010-07-14 11:09:33 +0600

    Удалил файл миграции schema/migrations/022_registration_deadline_migration.rb так как сушествует файл миграции с таким номером.

--- schema/migrations/022_registration_deadline_migration.rb
+++
@@ -1,9 +0,0 @@
-class RegistrationDeadlineMigration < ActiveRecord::Migration
-  def self.up
-    add_column :games, :registration_deadline, :datetime
-  end
-
-  def self.down
-    remove_column :games, :registration_deadline
-  end
-end
