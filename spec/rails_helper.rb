4f7d310211e8a8265f5c41b5b8d30a5014a75000
Daniel Vartanov <dan@vartanov.net>
2022-06-01 08:11:00 +0100

    Actually enable super-diff in tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ require_relative '../config/environment'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'super_diff/rspec-rails'
 
 require 'capybara/rails'
 Capybara.server = :puma, { Silent: true }
